11c2bd2dfbb18473078292ff1ad70f7d4f6e933e
Kevin Zhang <jiz100@ucsd.edu>
2022-11-14 21:45:31 -0800

    update 2nd

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -55,6 +55,7 @@ function initializeServiceWorker() {
     // B3. TODO - Register './sw.js' as a service worker (The MDN article
     //            "Using Service Workers" will help you here)
       const registration = await navigator.serviceWorker.register("./sw.js");
+      
     // B4. TODO - Once the service worker has been successfully registered, console
     //            log that it was successful.
       if(registration.active){
